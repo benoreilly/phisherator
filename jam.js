35960a279e54ef10f6742a0c76ad0fad488c886f
Ben OReillygit config --global user.email Ben <neb.oreilly@gmail.com>
2021-03-08 17:52:39 -0500

    m

--- jam.js
+++ jam.js
@@ -1,67 +1,60 @@
-
- // request jamcharts
- 
+// request jamcharts
 
 var jamChartSettings = {
-  "async": true,
-  "crossDomain": true,
+  async: true,
+  crossDomain: true,
   // "crossOrigin": anonymous,
-  "url": "https://api.phish.net/v3/jamcharts/all?apikey=824BE3C2264A913D97FA",
-  "method": "Get",
-  "headers": "{Access-Control-Allow-Origin: *}",
-  "data": "{}"
-}
-
-
+  url: "https://api.phish.net/v3/jamcharts/all?apikey=824BE3C2264A913D97FA",
+  method: "Get",
+  headers: "{Access-Control-Allow-Origin: *}",
+  data: "{}",
+};
 
 $.ajax(jamChartSettings).done(function (response) {
-    var jamInfo = response.response.data;
-    let jamCollection = document.querySelector('.collection');
+  var jamInfo = response.response.data;
+  console.log(jamInfo);
+  let jamCollection = document.querySelector(".collection");
 
-    for (let i = 0; i < jamInfo.length; i++) {
-        
-        const li = document.createElement('li');
-        li.className = 'collection-item songItem';
+  for (let i = 0; i < jamInfo.length; i++) {
+    const li = document.createElement("li");
+    li.className = "collection-item songItem";
 
-        let songItemSong = document.createElement('span');
-        songItemSong.className = 'songContext context';
+    let songItemSong = document.createElement("span");
+    songItemSong.className = "songContext context";
 
-        let songTitle = jamInfo[i].song;
-        songItemSong.textContent = songTitle;
-        li.appendChild(songItemSong);
+    let songTitle = jamInfo[i].song;
+    songItemSong.textContent = songTitle;
+    li.appendChild(songItemSong);
 
-        const link = document.createElement('a');
-        let songLink = jamInfo[i].link;
-        link.className = 'song-link secondary-content';
-        link.setAttribute('href', songLink);
-        link.setAttribute('target', '_blank');
-        link.innerHTML = '<i class="material-icons">launch</i>';
-        li.appendChild(link);
-       
-        
-        jamCollection.appendChild(li);
-      }
+    const link = document.createElement("a");
+    let songLink = jamInfo[i].link;
+    link.className = "song-link secondary-content";
+    link.setAttribute("href", songLink);
+    link.setAttribute("target", "_blank");
+    link.innerHTML = '<i class="material-icons">launch</i>';
+    li.appendChild(link);
 
+    jamCollection.appendChild(li);
+  }
 });
 
- // request recent setlist
+// request recent setlist
 
- let setListDate = document.querySelector('#setListDate');
- let setListVenue = document.querySelector('#setListVenue');
- 
- let setListLocation = document.querySelector('#setListLocation');
- let setListDetails = document.querySelector('#setListDetails');
- //let setListNotes = document.querySelector('#setListNotes');
- 
+let setListDate = document.querySelector("#setListDate");
+let setListVenue = document.querySelector("#setListVenue");
 
- var setlistSettings = {
-  "async": true,
-  "crossDomain": true,
-  "url": "https://api.phish.net/v3/setlists/latest?apikey=824BE3C2264A913D97FA",
-  "method": "Get",
-  "headers": "{Access-Control-Allow-Origin: *}",
-  "data": "{}"
-}
+let setListLocation = document.querySelector("#setListLocation");
+let setListDetails = document.querySelector("#setListDetails");
+//let setListNotes = document.querySelector('#setListNotes');
+
+var setlistSettings = {
+  async: true,
+  crossDomain: true,
+  url: "https://api.phish.net/v3/setlists/latest?apikey=824BE3C2264A913D97FA",
+  method: "Get",
+  headers: "{Access-Control-Allow-Origin: *}",
+  data: "{}",
+};
 
 $.ajax(setlistSettings).done(function (response) {
   let setListInfo = response.response.data[0];
@@ -71,8 +64,6 @@ $.ajax(setlistSettings).done(function (response) {
   let slLocation = setListInfo.location;
   let slDetails = setListInfo.setlistdata;
 
-  
-  
   setListDate.innerText = slDateLong;
 
   setListVenue.innerHTML = slVenue;
@@ -82,5 +73,4 @@ $.ajax(setlistSettings).done(function (response) {
   setListLocation.innerText = slLocation;
   setListDetails.innerHTML = slDetails;
   //setListNotes.innerHTML = slNotes;
-
 });
