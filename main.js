247ae08bf9302660ca11a047e515980d26661b5d
Ben OReillygit config --global user.email Ben <neb.oreilly@gmail.com>
2021-03-11 16:55:31 -0500

    removed cors anywhere

--- main.js
+++ main.js
@@ -56,11 +56,3 @@ function filterTasks(e) {
   }
   markFunction();
 }
-
-//CORS
-
-// jQuery.ajaxPrefilter(function (options) {
-//   if (options.crossDomain && jQuery.support.cors) {
-//     options.url = "https://cors-anywhere.herokuapp.com/" + options.url;
-//   }
-// });
